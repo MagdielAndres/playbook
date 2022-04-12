29783d8bf745189c5300522b88b1354fc4f4b3c3
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-12 17:26:28 -0500

    Agregando Ejemplo 2 de examples_0.js

--- weekly_mission_2/example0/example0-2.js
+++ weekly_mission_2/example0/example0-2.js
@@ -1,7 +1,7 @@
 // Ejemplo 2: Crear un objeto con propiedades
 const myObjetc2 = {
-    name: "Carlo",
-    age: 27
+    name: "Magdiel andrés",
+    age: 30
 }
 console.log("Ejemplo 2: Crear un objeto con propiedades")
 console.log(myObjetc2)
