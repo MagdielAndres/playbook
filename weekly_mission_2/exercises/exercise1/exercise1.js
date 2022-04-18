1cccbeaa433940832c701b702df41acd82fe781d
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-18 18:00:29 -0500

    Agregando el objeto issue con sus atributos, funciones

--- weekly_mission_2/exercises/exercise1/exercise1.js
+++ weekly_mission_2/exercises/exercise1/exercise1.js
@@ -13,8 +13,31 @@ const repo = {
     getGeneralInfo: function(){
       return `This repository ${this.name} was created by ${this.author}`
     }
-   }
+}
    
-   console.log("Nombre del repo:" + repo.name)
-   console.log("Issues totales: " + repo.getTotalIssues())
-   console.log(repo.getGeneralInfo())
+console.log("Nombre del repo:" + repo.name)
+console.log("Issues totales: " + repo.getTotalIssues())
+console.log(repo.getGeneralInfo()+"\n")
+
+// Objeto  de Issue,sus atributos, funciones
+
+const issue = {
+    title: "Ejercicio 1",
+    repositoryNameAssociated: "MagdielAndres",
+    status: "Activo",
+    numberOfComments: 34,
+    labels: 4,
+    author: "Magdiel Andres",
+    dateCreated: "18/04/2022-16:17 p.m.",
+    lastUpdated: "18/04/2022-23:17 p.m.",
+    getTitleAndAuthor: function() {
+        return `Title: ${this.title} and Author: ${this.author}`
+    },
+    getGeneralInfo: function(){
+        return `Repository Name Associated ${this.repositoryNameAssociated} and crate by ${this.author}`
+    }
+}
+
+console.log("Titulo del issue: " + issue.title);
+console.log("Title and author: " + issue.getTitleAndAuthor());
+console.log(issue.getGeneralInfo());
