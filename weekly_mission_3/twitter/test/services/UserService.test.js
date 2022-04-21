e31a208efeaba64958b7c1f6799fc4e39d3cda0c
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-21 15:09:29 -0500

    Requerimiento 2: Agregar un nuevo método estático en `UserService` llamado `getInfo` que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto.

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -1,3 +1,4 @@
+const User = require('../../app/models/User');
 const UserService = require('./../../app/services/UserService');
 
 describe('Test for UserService', () => { 
@@ -8,5 +9,15 @@ describe('Test for UserService', () => {
         expect(user.username).toBe("MagdielAndres");
         expect(user.user).toBe("Magdiel Andrés");
         expect(user.bio).not.toBeUndefined();
+    });
+
+    test('2. Get all user data in a list', () => { 
+        const user = UserService.create(1, "MagdielAndres", "Magdiel Andrés");
+        const userInfoInList = UserService.getInfo(user);
+
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("MagdielAndres");
+        expect(userInfoInList[2]).toBe("Magdiel Andrés");
+        expect(userInfoInList[3]).toBe("Sin bio");
     });
 })
