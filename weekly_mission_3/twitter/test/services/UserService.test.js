fbab5bd583fe4f7ce503934ced9367de9cf196d7
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-21 18:05:33 -0500

    se agrego el requerimineot 3 y 4 en el tes unitario

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -1,4 +1,3 @@
-const User = require('../../app/models/User');
 const UserService = require('./../../app/services/UserService');
 
 describe('Test for UserService', () => { 
@@ -19,5 +18,25 @@ describe('Test for UserService', () => {
         expect(userInfoInList[1]).toBe("MagdielAndres");
         expect(userInfoInList[2]).toBe("Magdiel Andrés");
         expect(userInfoInList[3]).toBe("Sin bio");
+    });
+
+    test('3. Update username', () => { 
+        const user = UserService.create(1, "MagdielAndres", "Magdiel Andrés");
+        UserService.updateUserUsername(user, "magdiel");
+        expect(user.username).toBe("magdiel");
+    });
+
+    //Requerimiento 4: Agregar un nuevo método estático en `UserService` llamado `getAllUsernames`, que recibirá una lista de objetos de la clase `User`, y regresará la lista de todos los usernames de dichos objetos
+    test('4. Give a list of users give me the list of username', () => { 
+        const user1 = UserService.create(1, "MagdielAndres1", "Magdiel Andrés");
+        const user2 = UserService.create(1, "MagdielAndres2", "Magdiel Andrés");
+        const user3 = UserService.create(1, "MagdielAndres3", "Magdiel Andrés");
+
+        const username = UserService.getAllUsernames([user1, user2, user3]);
+
+        expect(username).toContain("MagdielAndres1");
+        expect(username).toContain("MagdielAndres2");
+        expect(username).toContain("MagdielAndres3");
+
     });
 })
