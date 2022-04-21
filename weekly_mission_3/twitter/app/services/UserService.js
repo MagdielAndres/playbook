4ff68cced7aa7d3e259203fc74158d4f56c61e43
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-21 15:11:08 -0500

    Se agrego un metodo statico getInfo que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -4,6 +4,12 @@ class UserService {
     static create(id, username, user){
         return new User(id, username, user, "Sin bio");
     }
+
+    static getInfo(user){
+        return [user.id, user.username, user.user, user="Sin bio"];
+    }
 }
+
+
 
 module.exports = UserService;
