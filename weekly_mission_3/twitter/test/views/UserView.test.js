24d286d84fd418cf2e6a5c9ac0a77574121cb59a
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-24 17:01:38 -0500

    Requerimiento 4 : Verifica que se pueda crear un objeto `User`, al enviar un payload con las siguientes propiedades: `username`, `id` y `name`

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,28 +1,39 @@
-const User = require('../../app/models/User');
+const UserService = require('../../app/services/UserService');
 const UserView = require('./../../app/views/UserView');
 
 describe('Test for UserView', () => { 
 
     // 1. Valida que al enviar en el `payload` un valor `null`, obtenga un objeto con la llave `error` y el valor indique `payload no existe`.
-    test('Return an error object when try to creat a new user whit an null playload', () => { 
+    test('1. Return an error object when try to creat a new user whit an null playload', () => { 
         const payload = null;
         const result = UserView.createUser(payload);
         //https://jestjs.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/);
     });
 
-   // 2. Valida que un `payload` contenga en alguna de las llaves `username`, `name`, o `id` un valor en `null`. Si hay un valor `null` regresa un objeto con la llave `error` y que indique el texto: `necesitan tener un valor válido`.
-    test('Return an error object when try to create a new user with a paylod with invalid properties', () => {
-        const paload = {username: null, name: 12, id: "id"};
-        const result = UserView.createUser(paload);
-        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    // 2. Valida que un `payload` contenga en alguna de las llaves `username`, `name`, o `id` un valor en `null`. Si hay un valor `null` regresa un objeto con la llave `error` y que indique el texto: `necesitan tener un valor válido`.
+    test('2. Return an error object when try to create a new user with invalid properties', () => { 
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesita tener un valor válido/)
     });
 
-    // 3. Valida que un `payload` con algunas de las propiedades necesarias regrese un objeto con la llave `error` indicando `necesitan tener un valor válido`.
-    test('Return an error object when try to create a new user with a payload with missing properties', () => {
-        const payload ={username: "Magdiel"};
-        const result = UserView.createUser(payload);
 
-        expect(result.error).toMatch(/necesitan tener un valor válido/);
-    })
+    // 3. Valida que un `payload` con algunas de las propiedades necesarias regrese un objeto con la llave `error` indicando `necesitan tener un valor válido`.
+    test('3. Return an error object when try to create a new user whith a payload whith missing properties', () => { 
+        const payload = {username: "username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesita tener un valor válido/)
+    });
+
+
+    // 4. Verifica que se pueda crear un objeto `User`, al enviar un payload con las siguientes propiedades: `username`, `id` y `name`.
+    test('Create a user by a give valid payload', () => { 
+        const payload = { username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    });
 })
