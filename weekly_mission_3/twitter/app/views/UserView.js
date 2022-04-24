8e7c9e5fd0b120e53caf4f670ed6a9f34ee3f625
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-24 17:03:46 -0500

    Requerimineto 4: Verifica que se pueda crear un objeto `User`, al enviar un payload con las siguientes propiedades: `username`, `id` y `name`

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -3,12 +3,19 @@ const UserService = require('./../services/UserService')
 class UserView{
 
     static createUser(payload){
-        if(payload == null){
-            return {error: "payload no existe"};
-        }else if ((payload.username === null || typeof payload.username !== 'string') || payload.name === null || payload.id === null) {
-            return {error: "necesitan tener un valor válido"};
-        }  
+        
+        if(payload === null){            
+            return {error: "payload no existe"}
+        }else if((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.name !== 'string' || payload.name === null) || (typeof payload.id !== 'number' || payload.id === null)){
+            return {error: "necesita tener un valor válido"}
+        }else{
+            
+            return UserService.create(payload.id, payload.username, payload.name)
+        } 
     }
 }
 
+// const payload = { username: "username", id: 1, name: "name"}
+// const result = UserView.createUser(payload)
+// console.log(result)
 module.exports = UserView;
