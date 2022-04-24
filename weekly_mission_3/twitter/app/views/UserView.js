d77a57088033e62905fac86b597c743f296f0bd3
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-24 18:31:42 -0500

    Requerimiento 4: actualizando la propiedad name por user

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -1,21 +1,20 @@
 const UserService = require('./../services/UserService')
-
 class UserView{
 
     static createUser(payload){
         
         if(payload === null){            
             return {error: "payload no existe"}
-        }else if((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.name !== 'string' || payload.name === null) || (typeof payload.id !== 'number' || payload.id === null)){
+        }else if((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.user !== 'string' || payload.user === null) || (typeof payload.id !== 'number' || payload.id === null)){
             return {error: "necesita tener un valor válido"}
         }else{
             
-            return UserService.create(payload.id, payload.username, payload.name)
+            return UserService.create(payload.id, payload.username, payload.user)
         } 
     }
 }
 
-// const payload = { username: "username", id: 1, name: "name"}
+// const payload = { username: "username", id: 1, user: "name"}
 // const result = UserView.createUser(payload)
 // console.log(result)
 module.exports = UserView;
