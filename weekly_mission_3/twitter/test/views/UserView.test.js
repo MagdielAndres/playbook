a2417857ed2db5fa718af0c3756bfdaa08725a49
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-24 11:58:29 -0500

    Requerimiento 3: Valida que un `payload` con algunas de las propiedades necesarias regrese un objeto con la llave `error` indicando `necesitan tener un valor válido`

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,3 +1,4 @@
+const User = require('../../app/models/User');
 const UserView = require('./../../app/views/UserView');
 
 describe('Test for UserView', () => { 
@@ -14,6 +15,14 @@ describe('Test for UserView', () => {
     test('Return an error object when try to create a new user with a paylod with invalid properties', () => {
         const paload = {username: null, name: 12, id: "id"};
         const result = UserView.createUser(paload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    });
+
+    // 3. Valida que un `payload` con algunas de las propiedades necesarias regrese un objeto con la llave `error` indicando `necesitan tener un valor válido`.
+    test('Return an error object when try to create a new user with a payload with missing properties', () => {
+        const payload ={username: "Magdiel"};
+        const result = UserView.createUser(payload);
+
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     })
 })
