bbb20a1d135100fa7ab7d81e24d657881da54057
MagdielAndres <magdiel.vampire@gmail.com>
2022-04-24 11:59:53 -0500

    validando que el valor wur corresponda se avalida y si no que muestre el error

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -5,7 +5,7 @@ class UserView{
     static createUser(payload){
         if(payload == null){
             return {error: "payload no existe"};
-        }else if (payload.username === null || payload.name === null || payload.id === null) {
+        }else if ((payload.username === null || typeof payload.username !== 'string') || payload.name === null || payload.id === null) {
             return {error: "necesitan tener un valor válido"};
         }  
     }
